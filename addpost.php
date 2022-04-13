d746a234d25909712b42f44a442df0beb134c0ff
John <ashton.basile@gmail.com>
2022-04-13 16:23:59 +0200

    The add post functionality works now.

--- addpost.php
+++ addpost.php
@@ -4,7 +4,19 @@
 
 	// Check For Submit
 	if(isset($_POST['submit'])){
-		echo 'Submitted';
+		// Get form data
+		$title = mysqli_real_escape_string($conn,$_POST['title']);
+		$body = mysqli_real_escape_string($conn,$_POST['body']);
+		$author = mysqli_real_escape_string($conn,$_POST['author']);
+
+		$query = "INSERT INTO posts(title, author, body) VALUES('$title','$author','$body')";
+
+		if(mysqli_query($conn, $query)){
+			header('Location: '.ROOT_URL.'');
+		} 
+		else {
+			echo 'ERROR: '. mysqli_error($conn);
+		}
 	}
 ?>
 
